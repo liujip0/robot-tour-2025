cd5a1b61322b90ec31e2dbea3bb72cf06fbd9e35
liujip0 <liujip2020@gmail.com>
2025-03-31 09:32:45 -0400

    add first_forward function

--- robot-tour-2025.ino
+++ robot-tour-2025.ino
@@ -1,13 +1,22 @@
 #include <Romi32U4.h>
 #include <LSM6.h>
 
-char movements[] = {
-  'L'
+const char TURN_LEFT = 'L';
+const char TURN_RIGHT = 'R';
+const char FORWARD = 'F';
+const char BACK = 'B';
+
+const char movements[] = {
+  FORWARD,
+  FORWARD,
+  FORWARD,
+  FORWARD
 };
 
 Romi32U4Encoders encoders;
 Romi32U4Motors motors;
 Romi32U4ButtonA buttonA;
+Romi32U4ButtonB buttonB;
 Romi32U4Buzzer buzzer;
 LSM6 imu;
 
@@ -16,10 +25,12 @@ char report[80];
 int leftEncoder = 0;
 int rightEncoder = 0;
 
-int RIGHT_MOTOR_SPEED = 100;
-int LEFT_MOTOR_SPEED = round(1.077 * RIGHT_MOTOR_SPEED);
-int TURN_ENCODER_COUNTS = 770;
-int FORWARD_ENCODER_COUNTS = 3250;
+const int RIGHT_MOTOR_SPEED = 100;
+const int LEFT_MOTOR_SPEED = round(1.0595 * RIGHT_MOTOR_SPEED); // 1.077 // 1.065 // 1.0655
+const int RIGHT_TURN_ENCODER_COUNTS = 737;
+const int LEFT_TURN_ENCODER_COUNTS = 739;
+const int FORWARD_ENCODER_COUNTS = 3250;
+const int FIRST_FORWARD_ENCODER_COUNTS = round(0.6 * FORWARD_ENCODER_COUNTS);
 
 void setup() {
   // put your setup code here, to run once:
@@ -33,14 +44,30 @@ void setup() {
   imu.g.x;
 
   Serial.begin(9600);
-
-  buttonA.waitForPress();
-  buttonA.waitForRelease();
-  forward();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // Serial.write("");
+  // Serial.println();
+  buttonA.waitForPress();
+  buttonA.waitForRelease();
+  for (int i = 0; i < sizeof(movements); i++) {
+    switch (movements[i]) {
+      case FORWARD:
+        forward();
+        break;
+      case BACK:
+        back();
+        break;
+      case TURN_LEFT:
+        left();
+        break;
+      case TURN_RIGHT:
+        right();
+        break;
+    }
+  }
 }
 
 void forward() {
@@ -57,6 +84,34 @@ void forward() {
   motors.setSpeeds(0, 0);
 }
 
+void first_forward() {
+  leftEncoder = encoders.getCountsAndResetLeft();
+  rightEncoder = encoders.getCountsAndResetRight();
+  leftEncoder = 0;
+  rightEncoder = 0;
+  motors.setSpeeds(LEFT_MOTOR_SPEED, RIGHT_MOTOR_SPEED);
+  buzzer.play("C");
+  while (leftEncoder < FIRST_FORWARD_ENCODER_COUNTS && rightEncoder < FIRST_FORWARD_ENCODER_COUNTS) {
+    leftEncoder = encoders.getCountsLeft();
+    rightEncoder = encoders.getCountsRight();
+  }
+  motors.setSpeeds(0, 0);
+}
+
+void back() {
+  leftEncoder = encoders.getCountsAndResetLeft();
+  rightEncoder = encoders.getCountsAndResetRight();
+  leftEncoder = 0;
+  rightEncoder = 0;
+  motors.setSpeeds(-LEFT_MOTOR_SPEED, -RIGHT_MOTOR_SPEED);
+  buzzer.play("D");
+  while (leftEncoder > -FORWARD_ENCODER_COUNTS && rightEncoder > -FORWARD_ENCODER_COUNTS) {
+    leftEncoder = encoders.getCountsLeft();
+    rightEncoder = encoders.getCountsRight();
+  }
+  motors.setSpeeds(0, 0);
+}
+
 void right() {
   leftEncoder = encoders.getCountsAndResetLeft();
   rightEncoder = encoders.getCountsAndResetRight();
@@ -65,13 +120,34 @@ void right() {
   if (leftEncoder == 0) {
     buzzer.play("G");
   }
-  motors.setSpeeds(RIGHT_MOTOR_SPEED, -RIGHT_MOTOR_SPEED);
+  motors.setSpeeds(LEFT_MOTOR_SPEED, -RIGHT_MOTOR_SPEED);
   if (leftEncoder > 0) {
     buzzer.play("A");
   } else {
     buzzer.play("B");
   }
-  while (leftEncoder < TURN_ENCODER_COUNTS && rightEncoder > -TURN_ENCODER_COUNTS) {
+  while (leftEncoder < RIGHT_TURN_ENCODER_COUNTS && rightEncoder > -RIGHT_TURN_ENCODER_COUNTS) {
+    leftEncoder = encoders.getCountsLeft();
+    rightEncoder = encoders.getCountsRight();
+  }
+  motors.setSpeeds(0, 0);
+}
+
+void left() {
+  leftEncoder = encoders.getCountsAndResetLeft();
+  rightEncoder = encoders.getCountsAndResetRight();
+  leftEncoder = 0;
+  rightEncoder = 0;
+  if (leftEncoder == 0) {
+    buzzer.play("F");
+  }
+  motors.setSpeeds(-LEFT_MOTOR_SPEED, RIGHT_MOTOR_SPEED);
+  if (leftEncoder >= 0) {
+    buzzer.play("A");
+  } else {
+    buzzer.play("B");
+  }
+  while (leftEncoder > -LEFT_TURN_ENCODER_COUNTS && rightEncoder < LEFT_TURN_ENCODER_COUNTS) {
     leftEncoder = encoders.getCountsLeft();
     rightEncoder = encoders.getCountsRight();
   }
